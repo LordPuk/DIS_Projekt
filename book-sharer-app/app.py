8bc80b652c562126c419b00073f889b1b197dde8
Nollerberg <93582729+Nollerberg@users.noreply.github.com>
2023-06-08 16:25:33 +0200

    Login, logout, create user, view my books, see book details (- genres) works

--- book-sharer-app/app.py
+++ book-sharer-app/app.py
@@ -13,73 +13,104 @@ from datetime import date
 app = Flask(__name__ , static_url_path='/static')
 
 # set your own database name, username and password
-db = "dbname='qianq' user='qianq' host='localhost' password='psql'" #potentially wrong password
+db = "dbname='postgres' user='postgres' host='localhost' password=''" #potentially wrong password
 conn = psycopg2.connect(db)
 cursor = conn.cursor()
 
 
-#Kode for login system ..
-#
-#
-#kom nu!!!!
 
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def account():
-    cur = conn.cursor()
-    if request.method == "POST":
-            user = request.form["createuser"]
-            password = request.form["createpassword"]
-            cur.execute("SELECT username FROM users WHERE username LIKE %s AND password LIKE %s", (user, password))
-            unique = cur.fetchall()
-            if len(unique) != 0:
-                return redirect(url_for("home"))
-            else:
-                flash("wrong pass or name. how about DNUR")
-            
-    return render_template("login.html")
+    return render_template("index.html")
 
 
-@app.route("/create", methods=["POST"])
+@app.route("/createuser", methods=["GET","POST"])
 def create():
     cur = conn.cursor()
     if request.method == "POST":
         user = request.form["createuser"]
         password = request.form["createpassword"]
-        cur.execute("SELECT username FROM users WHERE username LIKE %s", (user))
+        language = request.form["Language"]
+        education = request.form["Education"]
+        cur.execute(f'''SELECT username FROM users WHERE username LIKE '{user}' ''')
         unique = cur.fetchall()
+        print(unique)
         if len(unique) == 0:
             today = date.today()
             cur.execute("INSERT INTO users(Username, Password, Creation_date) VALUES (%s, %s, %s)", (user, password, today))
+            cur.execute("INSERT INTO Readers(Username, Education, Language) VALUES (%s, %s, %s)", (user, education, language))
+            flash("Account created.")
             conn.commit()
-            return redirect(url_for("home"))
+            return redirect(url_for("my_books"))
     return render_template("create_user.html")
                 
 
 
-@app.route("/book_details")
+@app.route("/bookdetails", methods=["POST","GET"])  
 def book_details():
-    return render_template("book_details.html")
+    cur= conn.cursor()
+    isbn = request.args.get('isbn',default =0, type = str)
+    cur.execute(f'''SELECT * FROM Books WHERE ISBN LIKE '{isbn}' ''')
+    data = cur.fetchall()
+    cur.execute(f'''SELECT * FROM Has_genre WHERE ISBN LIKE '{isbn}' ''')
+    genres = cur.fetchall()
+    if request.method =="POST":
+            print("KNAP TRYKKET")
+            if session.get('logged_in'):
+                print("LOGGET IND OG GEM")
+                user = session['username']
+                today = date.today()
+                cur.execute("INSERT INTO Reads(Username, ISBN, Start_date, Completion_date, Current_pages) VALUES (%s, %s, %s, NULL, 0)", (user, isbn, today)) #bog ind i 
+                cur.commit()
+            else:
+                flash("cannot save book if not logged in lul dnur")
+
+    return render_template("book_details.html", data = data)
+ 
 
 @app.route("/discover")
-def discvoer():
+def discover():
     return render_template("discover.html")
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
 
-@app.route("/login")
-def login():
+@app.route("/login", methods=["GET","POST"])
+def index():
+    cur = conn.cursor()
+    if session.get('logged_in') :
+        return redirect(url_for("account"))
+    if request.method == "POST":
+            user = request.form["user"]
+            password = request.form["password"]
+            cur.execute("SELECT username FROM users WHERE username LIKE %s AND password LIKE %s", (user, password))
+            unique = cur.fetchall()
+            if len(unique) != 0:
+                session['logged_in'] = True
+                session['username'] = user
+                return redirect(url_for("my_books", name = user))
+            else:
+                flash("wrong pass or name. how about DNUR")
     return render_template("login.html")
+
 
 @app.route("/logout")
 def logout():
+    session['logged_in'] = False
+    session['username'] = ""
     return render_template("logout.html")
 
 @app.route("/mybooks")
 def my_books():
-    return render_template("mybooks.html")
+    cur = conn.cursor()
+    if not session.get('logged_in') :
+        return redirect(url_for("account"))
+    else:
+        user = session['username']
+        cur.execute(f'''SELECT ISBN, Title, Author, (ISBN IN (SELECT ISBN FROM Favorite_book WHERE Username LIKE '{user}')) AS Favorite FROM (Books NATURAL JOIN Reads) WHERE Username = '{user}';''')
+        data = cur.fetchall()
+        #print(data)
+        
+        return render_template("mybooks.html", name=session['username'],len = len(data), data = data)
+    
 
 @app.route("/ratings")
 def ratings():
